f95dca98984980bfcf92d3862dc6662dcac0ea2c
José Felipe Ribeiro <felipe.brx.dev@gmail.com>
2021-11-12 11:35:06 -0300

    Alteracao nome class

--- Logica/main.py
+++ Logica/main.py
@@ -1,4 +1,4 @@
-class ServidorVirtual(object):
+class Balanceamento_Carga(object):
     def __init__(self):
         self.tick_por_tarefa = 0
         self.max_users_servidor = 0
@@ -13,5 +13,10 @@ class ServidorVirtual(object):
         print("Número máximo de ticks por tarefa: ", self.tick_por_tarefa)
         print("Número máximo de usuários por servidores virtuais: ", self.max_users_servidor)
 
+    def config_(self):
+        with open("input.txt",'r') as entrada:
+            self.tick_por_tarefa = (entrada.readline(2).rstrip("\n"))
+            self.max_users_servidor = (entrada.readline(1).rstrip("\n"))
 
-ServidorVirtual()
+
+Balanceamento_Carga()
