aff8cedc766fd741937c117813e0de66d42f82c8
José Felipe Ribeiro <felipe.brx.dev@gmail.com>
2021-11-13 18:03:22 -0300

    Realizando mais testes automatizados com pycharm

--- Logica/COMMENTS.md
+++ Logica/COMMENTS.md
@@ -1,4 +1,29 @@
 # SOBRE O PROJETO
 
 Decide utilizar a estrutura de programação orientada a objetos,
-por ser mais facil de manipudar variaveis e criar objetos para interagir com eles
+por ser mais facil de manipudar variaveis e criar objetos para interagir com eles.
+
+12/11/21 7:31: <br>
+Elaborando estrutura do projeto
+
+Estrutura:
+--> Classe: Balanceamento_Carga
+    <br>
+        -->Metodos: __init__, Config_Servidor, Entrada_De_Usuarios, Balancear_Carga
+
+Os teste otimizados serão realizados com pycharm.
+
+12/11/21 11:34<br>
+Criando método Entrada_De_Usuarios para ler arquivo input.txt e incluir todos os dados no atributo tipo 
+lista "input_usuarios".
+
+
+
+13/11/21 09:12:
+Crie dois metodos para realizar a alocação dos usuários nos servidores.<br>
+Balancear_Carga e Balancear_Carga1
+
+13/11/21 17:25:
+Criando as primeiras otimizações:
+ - Verficando código repetido.  
+ - Primeiros testes em massa pelo arquivos input.txt
--- Logica/main.py
+++ Logica/main.py
@@ -6,16 +6,16 @@ class Balanceamento_Carga(object):
         self.servidores = 0
         self.server = {}
         self.server_lis = []
-        self.usuarios = []
+        self.input_usuarios = []
 
         self.total_users = 0
 
-        self.config_servidor()
+        self.Config_Servidor()
         self.Balancear_Carga()
 
 
 
-    def config_servidor(self):
+    def Config_Servidor(self):
         with open("input.txt",'r') as entrada:
             self.tick_por_tarefa = (entrada.readline(2).rstrip("\n"))
             self.max_users_servidor = (entrada.readline(1).rstrip("\n"))
@@ -23,89 +23,88 @@ class Balanceamento_Carga(object):
         print("Número máximo de ticks por tarefa: ", self.tick_por_tarefa)
         print("Número máximo de usuários por servidores virtuais: ", self.max_users_servidor)
 
-    def entrada_de_usuarios(self):
+    def Entrada_De_Usuarios(self):
         with open("input.txt",'r') as arquivo:
             self.tick_por_tarefa = (arquivo.readline(2).rstrip("\n"))
             self.max_users_servidor = (arquivo.readline(1).rstrip("\n"))
             linha = 0
             for usuarios in arquivo.readlines():
                 linha += 1
-                if linha > 2:
-                    self.usuarios.append( int(usuarios.rstrip("\n")))
+                if linha > 1:
+                    self.input_usuarios.append( int(usuarios.rstrip("\n")))
                     self.total_users = int(usuarios.rstrip("\n")) + self.total_users
 
 
-    def listar(self):
-        cont = 0
-        for users in range(self.total_users):
 
-            if cont == 2:
-                self.server_lis.append(users)
-                cont = 0
-            else:
-                #self.server_lis[0] = cont
-                cont += 1
-    def Balancear_Carga_2(self):
-        self.entrada_de_usuarios()
+    def Balancear_Carga(self):
+        self.Entrada_De_Usuarios()
         entrada_users = 0
         linha = 0
         tarefas = 0
         lista = [0]
-
-        for x in range(self.total_users+ 4):
+        #print(self.input_usuarios)
+        for x in range(10):
             linha +=1
             cont = 0
+            if x<len(self.input_usuarios):
+                print('|', x + 1, "    | ", self.input_usuarios[x], "              | ", self.servidores)
 
-            while entrada_users > 0:
+                print(self.input_usuarios[x])
+                entrada_users = self.input_usuarios[x]
+                while entrada_users > 0:
+                    #print(self.input_usuarios)
+                    if entrada_users == 1:
 
-                if entrada_users == 1:
-                    # print("lista[-1] ",lista[-1])
-                    if (lista[-1] == 1) or (lista[-1] == 0):
-                        lista[-1] = lista[-1] + 1
-                        # print(" valor add ", lista)
-                        entrada_users = entrada_users - 1
-                        # print("lista[-1] ", lista)
-                    else:
-                        lista.append(1)
-                        # print("lista[-1] ", lista)
-                        print(" 1 user atuais tt", entrada_users)
-                        entrada_users = entrada_users - 1
+                        if (lista[-1] == 1) or (lista[-1] == 0):
+                            lista[-1] = lista[-1] + 1
+                            # print(" valor add ", lista)
+                            entrada_users = entrada_users - 1
+                            # print("lista[-1] ", lista)
+                            #print("lista----", lista)
+                        else:
+                            lista.append(1)
+                            # print("lista[-1] ", lista)
+                            #print(" 1 user atuais tt", entrada_users)
+                            entrada_users = entrada_users - 1
+                            #print("lista----", lista)
 
-                elif entrada_users >= 2:
-                    if (lista[-1] == 1):
-                        lista[-1] = lista[-1] + 1
-                        print(" valor add ", lista)
-                        entrada_users = entrada_users - 1
-                        # print("lista[-1] ", lista)
-                    elif lista[-1] == 2:
-                        lista.append(2)
-                        # print("lista[-1] ", lista)
-                        print(" 1 user atuais tt", entrada_users)
-                        entrada_users = entrada_users - 2
+                    elif entrada_users >= 2:
+                        if (lista[-1] == 1):
+                            lista[-1] = lista[-1] + 1
+                            #print(" valor add ", lista)
+                            entrada_users = entrada_users - 1
+                            # print("lista[-1] ", lista)
+                            #print("lista----", lista)
+                        elif lista[-1] == 2:
+                            lista.append(2)
+                            # print("lista[-1] ", lista)
+                            #print(" 1 user atuais tt", entrada_users)
+                            entrada_users = entrada_users - 2
+                            #print("lista----", lista)
 
-
-
-            print("lista----",lista)
+            self.servidores = lista.count(2) + lista.count(1)
+            #print("lista----",lista)
             tarefas += 1
             print("contador de ticks: ", tarefas)
             if tarefas == 4:
                 print("removido servidor...")
                 tarefas = 0
 
-            print(self.server_lis)
-            print(self.server)
+            #print(self.server_lis)
+            #print(self.server)
             print("---------------------------------------------------------")
             print("| Tick  | usuários logados | Servidores")
-            if x<len(self.usuarios):
-                print('|', x + 1, "    | ", self.usuarios[x], "              | ", self.servidores)
+            if x<len(self.input_usuarios):
+                print('|', x + 1, "    | ", self.input_usuarios[x], "              | ", self.servidores)
             else:
                 print('|', x+1, "   | ",'---', "            | ", self.servidores)
             print("---------------------------------------------------------")
             print("lista ", lista)
+            print("--------------------------------------------------------")
 
 
 
-    def Balancear_Carga(self):
+    def Balancear_Carga1(self):
         entrada_users = 0
         lista = [0]
         cont = 0
@@ -113,7 +112,7 @@ class Balanceamento_Carga(object):
         self.entrada_de_usuarios()
         with open("input.txt",'r') as arquivo:
             linha  = 0
-            #self.usuarios
+            #self.input_usuarios
             for usuarios in arquivo.readlines():
                 linha +=1
 
