2d575dda2a63cd2eadb5acb85d3c6c40aadc3232
José Felipe Ribeiro <felipe.brx.dev@gmail.com>
2021-11-13 20:09:35 -0300

    Criando mecanismo de remocao de usuarios quando atinge seu limite de tarefas

--- Logica/main.py
+++ Logica/main.py
@@ -42,14 +42,14 @@ class Balanceamento_Carga(object):
         linha = 0
         tarefas = 0
         lista = [0]
+        user_remover_list = self.input_usuarios
         #print(self.input_usuarios)
         for x in range(len(self.input_usuarios)+4):
             linha +=1
             cont = 0
             if x<len(self.input_usuarios):
 
-
-                print('Entrou: ',self.input_usuarios[x])
+                print('Entrou ',self.input_usuarios[x], ' usuarios')
                 entrada_users = self.input_usuarios[x]
                 while entrada_users > 0:
                     #print(self.input_usuarios)
@@ -82,13 +82,25 @@ class Balanceamento_Carga(object):
                             entrada_users = entrada_users - 2
                             #print("lista----", lista)
 
+            else:
+                print('Entrou 0 usuarios...')
+
             self.servidores = lista.count(2) + lista.count(1)
             #print("lista----",lista)
             tarefas += 1
             print("contador de ticks: ", tarefas)
+
             if tarefas == 4:
-                print("removido servidor...")
+                #print("removido servidor...")
                 tarefas = 0
+
+                users_remover = user_remover_list[0]
+                print("users a serem removidos: ",self.input_usuarios[0])
+                while users_remover > 0:
+                    print("removido")
+                    users_remover = users_remover - 1
+                print(user_remover_list.pop(0))
+
 
             #print(self.server_lis)
             #print(self.server)
@@ -101,76 +113,6 @@ class Balanceamento_Carga(object):
             print("---------------------------------------------------------")
             print("lista ", lista)
             print("--------------------------------------------------------")
-
-
-
-    def Balancear_Carga1(self):
-        entrada_users = 0
-        lista = [0]
-        cont = 0
-        entre_user_list =[]
-        self.entrada_de_usuarios()
-        with open("input.txt",'r') as arquivo:
-            linha  = 0
-            #self.input_usuarios
-            for usuarios in arquivo.readlines():
-                linha +=1
-
-
-                if linha > 2:
-                    self.total_users =  int(usuarios.rstrip("\n")) + self.total_users
-                    entrada_users += int(usuarios.rstrip("\n"))
-                    entre_user_list.append(int(usuarios.rstrip("\n")))
-                    self.tick_por_tarefa = (arquivo.readline(2).rstrip("\n"))
-                    self.max_users_servidor = (arquivo.readline(1).rstrip("\n"))
-                    cont = cont + 1
-                    print("users atual : ", entrada_users)
-
-                    if cont == 4:
-                        user_remove = entre_user_list[0]
-                        print("remover usuarios: ",user_remove)
-                        cont = 0
-
-
-                    while entrada_users > 0:
-
-                        if entrada_users == 1:
-                            #print("lista[-1] ",lista[-1])
-                            if (lista[-1] == 1) or (lista[-1] == 0):
-                                lista[-1] = lista[-1] + 1
-                                #print(" valor add ", lista)
-                                entrada_users = entrada_users - 1
-                                #print("lista[-1] ", lista)
-                            else:
-                                lista.append(1)
-                                #print("lista[-1] ", lista)
-                                print(" 1 user atuais tt", entrada_users)
-                                entrada_users = entrada_users - 1
-
-                        elif entrada_users >= 2:
-                            if (lista[-1] == 1):
-                                lista[-1] = lista[-1] + 1
-                                print(" valor add ", lista)
-                                entrada_users = entrada_users - 1
-                                #print("lista[-1] ", lista)
-                            elif lista[-1] == 2:
-                                lista.append(2)
-                                #print("lista[-1] ", lista)
-                                print(" 1 user atuais tt", entrada_users)
-                                entrada_users = entrada_users - 2
-
-                    self.servidores = lista.count(2) + lista.count(1)
-                    print("lista[-1] atual ", lista)
-                    #print(self.server_lis)
-                    #print(self.server)
-                    print("---------------------------------------------------------")
-                    print("| Tick  | usuários | Servidores")
-                    print('|', linha - 2, "    | ", usuarios.rstrip("\n"), "      | ",self.servidores )
-                    print("---------------------------------------------------------")
-                    print("lista--- ", lista)
-
-            print("users: ",self.total_users)
-            self.listar()
 
 
 
