9f1dea7f6e7e6cc7085870ec0c1faceecde412f2
José Felipe Ribeiro <felipe.brx.dev@gmail.com>
2021-11-18 19:19:28 -0300

    Melhorando o codigo e o layout v3

--- Sistema/Portal_Scale_News/Portal_Scale_News/settings.py
+++ Sistema/Portal_Scale_News/Portal_Scale_News/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k!or&ycl7=bv4gv36^#3shogvs)84&c^h8c#8lo^ta=3hb3vkg'
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=True, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
